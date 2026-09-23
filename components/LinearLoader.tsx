40632a09da274bf149bdcc876458788ae2fd9676
AmirAli Shabani <iamamirali.sh@gmail.com>
2026-09-23 22:16:26 +0330

    style: increase stroke of linearLoader

--- components/LinearLoader.tsx
+++ components/LinearLoader.tsx
@@ -1,7 +1,7 @@
 export function LinearLoader({ show }: { show: boolean }) {
   if (show) {
     return (
-      <div className="fixed top-0 left-0 bg-emerald-100 z-9999 h-1 w-full overflow-hidden">
+      <div className="fixed top-0 left-0 bg-emerald-100 z-9999 h-1.5 w-full overflow-hidden">
         <div className="h-full w-1/2 animate-browser-loader rounded-full bg-emerald-500" />
       </div>
     );
