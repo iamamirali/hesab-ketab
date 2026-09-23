54f853cb29ee61fc2b5189a633d26c2939fb787f
AmirAli Shabani <iamamirali.sh@gmail.com>
2026-09-23 20:12:22 +0330

    feat: add root app loading file

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { LoadingSpinner } from "@/components/LoadingSpinner";
 
 export default function Loading() {
   return (
-    <div className="flex h-full items-center justify-center">
+    <div className="flex grow items-center justify-center">
       <LoadingSpinner />
     </div>
   );
